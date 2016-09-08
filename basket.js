24a0e5ab36f43c4840570f879d881612e71f6528
Iain Stewart <iain.stewart08@gmail.com>
2016-09-08 15:50:47 +0100

    added test for removing item

--- basket.js
+++ basket.js
@@ -7,6 +7,15 @@ Basket.prototype = {
   addItem: function(item) {
     this.items.push(item);
   },
+  removeItem: function(name) {
+    var items = this.items;
+    for (item of items) {
+      if (item.name === name) {
+        var index = items.indexOf(item);
+        items.splice(index, 1);
+      }
+    }
+  },
 }
 
 module.exports = Basket;
--- specs/basket_spec.js
+++ specs/basket_spec.js
@@ -3,14 +3,24 @@ var Item = require('../item');
 var assert = require('assert');
 
 describe('basket', function() {
+
   it('should initialise with zero items', function() {
     var basket = new Basket();
     assert.equal(0, basket.items.length);
   });
+
   it('should be able to add items', function() {
     var basket = new Basket();
     var item = new Item({name: 'Almond Toe Court Shoes, Patent Black', category: 'Women’s Footwear', price: 99.00, quantity: 5});
     basket.addItem(item);
     assert.deepEqual(item, basket.items[0]);
   });
+
+  it('should be able to remove items', function() {
+    var basket = new Basket();
+    var item = new Item({name: 'Almond Toe Court Shoes, Patent Black', category: 'Women’s Footwear', price: 99.00, quantity: 5});
+    basket.removeItem(item);
+    assert.equal(0, basket.items.length);
+  });
+
 });
