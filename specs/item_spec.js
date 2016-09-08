c339c8d613ab04b592fa06ba7dd406eb2c108e98
Iain Stewart <iain.stewart08@gmail.com>
2016-09-08 15:41:24 +0100

    added test for item category

--- specs/item_spec.js
+++ specs/item_spec.js
@@ -2,8 +2,15 @@ var Item = require('../item');
 var assert = require('assert');
 
 describe('item', function() {
+
   it('should have a name', function() {
     var item = new Item({name: 'Almond Toe Court Shoes, Patent Black', category: 'Women’s Footwear', price: 99.00, quantity: 5});
     assert.equal('Almond Toe Court Shoes, Patent Black', item.name);
   });
+
+  it('should have a category', function() {
+    var item = new Item({name: 'Almond Toe Court Shoes, Patent Black', category: 'Women’s Footwear', price: 99.00, quantity: 5});
+    assert.equal('Women’s Footwear', item.category);
+  });
+
 });
